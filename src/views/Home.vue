20b5c407aef3d8cf8bc25e09db5e96a8c4bd22c4
Iqsam Bashir <Iqsam.Bashir@bqe.com>
2024-01-02 15:18:28 +0530

    display letter and set the route for it

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,15 @@
 <template>
- <h2>home component</h2>
- <pre>{{meals}}</pre>
+<div class="flex flex-col p-8">
+    <input type="text" class="rounded border-2 border-gray-200 w-full" placeholder="Search For Meals" />
+    <div class="flex justify-center gap-2 mt-2">
+        <router-link :to="{name: 'byLetter' , params: {letter}}" v-for="letter of letters" :key="letter">
+            {{ letter }}
+        </router-link>
 
+    </div>
+</div>
 </template>
 
 <script setup>
-import {computed} from 'vue';
-import store from '../store';
-
-const meals = computed(() => store.state.meals)
-
-
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("");
 </script>
