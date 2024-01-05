dca0c59e54c4de80edac93c88ad3a9ba044a1526
Iqsam Bashir <Iqsam.Bashir@bqe.com>
2024-01-05 16:14:02 +0530

    display search results based on the keyword by user

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -21,14 +21,21 @@
 
 <script setup>
 import {computed} from '@vue/reactivity';
-import {ref} from 'vue';
+import {onMounted, ref} from 'vue';
+import {useRoute} from "vue-router";
 import store from '../store';
 
 const keyword = ref('');
-
+const route = useRoute();
 const meals = computed(() => store.state.searchedMeals)
 
 function searchMeals() {
     store.dispatch('searchMeals', keyword.value)
 }
+onMounted(() => {
+    keyword.value = route.params.name // if we write meal name in url it will come up in the search field
+    if(keyword.value){     //when it gets the meal name, it then call the searchMeal and brings the data of the particular meal
+        searchMeals();
+    }
+})
 </script>
