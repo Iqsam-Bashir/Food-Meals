2875bd4f19968336b3183960dc5ea7c1068d2f77
Iqsam Bashir <Iqsam.Bashir@bqe.com>
2024-01-08 15:19:26 +0530

    inner detail meal page

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Home from '../views/Home.vue';
 import MealsByIngredient from '../views/MealsByIngredient.vue';
 import MealsByLetter from '../views/MealsByLetter.vue';
 import MealsByName from '../views/MealsByName.vue';
+import MealDetails from '../views/MealDetails.vue';
 const routes=[
     {
         path: "/",
@@ -30,6 +31,12 @@ const routes=[
                 path: "/by-ingredient/:ingredient?",
                 name: "byIngredient",
                 component: MealsByIngredient,
+        
+            },
+            {
+                path: "/meal/:id",
+                name: "mealDetails",
+                component: MealDetails,
         
             },
         ]
--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,18 +1,18 @@
 <template>
-<div class="p-8">
+<div class="p-8 pb-0">
     <input type="text" v-model="keyword" class="rounded border-2 border-gray-200 w-full" placeholder="Search For Meals" @change="searchMeals" />
 
 </div>
 <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
-    <div v-for="meal of meals" :key="idMeal" class="bg-white shadow rounded-xl">
-      <router-link to="/">
+    <div v-for="meal of meals" :key="meal.idMeal" class="bg-white shadow rounded-xl">
+      <router-link :to="{name: 'mealDetails',  params: {id: meal.idMeal}}">
         <img :src="meal.strMealThumb" :alt="strMeal" class="rounded-t-xl w-full h-48 object-cover" />
       </router-link>  
         <div class="p-3">
             <h3 class="font-bold">{{meal.strMeal}}</h3>
             <p>Lorem ipsum dolor sit amet cque iusto nobis vero possimus impedit officiis, numquam eveniet magni tenetur.</p>
             <div class="flex items-center justify-between">
-                <a :href="meal.strYoutube" target="_blank" class="px-3 py-2 rounded border-2 border-red-500 bg-red-500 text-white hover:bg-red-400 transition-colors">Youtube</a>
+                <YoutubeButton :href="meal.strYoutube" />
             </div>
         </div>
     </div>
@@ -24,6 +24,7 @@ import {computed} from '@vue/reactivity';
 import {onMounted, ref} from 'vue';
 import {useRoute} from "vue-router";
 import store from '../store';
+import YoutubeButton from '../components/YoutubeButton.vue';
 
 const keyword = ref('');
 const route = useRoute();
